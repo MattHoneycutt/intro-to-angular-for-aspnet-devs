af544acb68a4070cf79cf30e4228f5a490a16a43
Matt Honeycutt <mbhoneycutt@gmail.com>
2016-06-04 15:02:16 -0500

    Added display of a single zombie.

--- src/IntroToAngularjsForAspnetDevs.Web/client/zombieIncidentList.component.js
+++ src/IntroToAngularjsForAspnetDevs.Web/client/zombieIncidentList.component.js
@@ -2,7 +2,19 @@
 
     angular.module('app')
         .component('zombieIncidentList', {
-            templateUrl: '/client/zombieIncidentList.tmpl.html'
+            templateUrl: '/client/zombieIncidentList.tmpl.html',
+            controller: controller,
+            controllerAs: 'vm'
         });
 
+    function controller() {
+        var vm = this;
+
+        vm.zombie = {
+            location: '327 Springs Road',
+            incidentType: 'smallSwarm',
+            reportedBy: 'Frances Moreno',
+            reportedAt: '2015-09-12T20:26:14Z'
+        };
+    }
 })();
