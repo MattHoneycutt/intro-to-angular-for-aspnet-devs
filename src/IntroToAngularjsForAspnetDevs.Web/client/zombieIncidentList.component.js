8cf4f1a88105928be6aa3ac8c473e2a7a717f4d7
Matt Honeycutt <mbhoneycutt@gmail.com>
2016-06-05 17:11:16 -0500

    Phase 2: got demo showing a single zombie.

---
+++ src/IntroToAngularjsForAspnetDevs.Web/client/zombieIncidentList.component.js
@@ -0,0 +1,20 @@
+﻿(function (undefined) {
+
+    angular.module('app')
+        .component('zombieIncidentList', {
+            templateUrl: '/client/zombieIncidentList.tmpl.html',
+            controller: controller,
+            controllerAs: 'vm'
+        });
+
+    function controller() {
+        var vm = this;
+
+        vm.zombie = {
+            location: '327 Springs Road',
+            incidentType: 'smallSwarm',
+            reportedBy: 'Frances Moreno',
+            reportedAt: '2015-09-12T20:26:14Z'
+        };
+    }
+})();
